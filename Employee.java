94aec0c538a93ce6767f126d6e22d6a851ea9bbc
ArnauldDyor <44143401+ArnauldDyor@users.noreply.github.com>
2019-02-20 05:23:53 +0100

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,14 @@
+package firstcase;
+
+import java.util.Date;
+import java.lang.String;
+
+public class Employee{
+    String name;
+    char sex;
+    Date birthDate;
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Service.java
@@ -0,0 +1,18 @@
+package firstcase;
+
+import java.lang.String;
+import java.util.Date;
+
+public class Service{
+    String serviceName;
+
+    public class Employee{
+        String name;
+        char sex;
+        Date birthDate;
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
